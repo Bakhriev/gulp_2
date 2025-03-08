48f598f10615fcef4ee38481d12e4e88618c0aa2
Bakhriev <legendofpain5112@gmail.com>
2025-03-08 06:19:18 +0500

    fix crash on error

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,9 +2,13 @@ import { src, dest } from "gulp";
 import { distPath, paths } from "../config/paths.js";
 import browserSync from "browser-sync";
 
-export const scripts = () => {
+export const scripts = async () => {
+	const plumber = (await import("gulp-plumber")).default;
+
 	return (
 		src(paths.scripts)
+			.pipe(plumber())
+			//
 			.pipe(dest(`${distPath}/js/`))
 			//
 			.pipe(browserSync.stream())
